48d0723af958af6b9f1584f9a73361b8ce595f57
vladimir9255 <vovochkaperepelkin@yandex.ru>
2020-11-24 05:19:19 +0900

    fix environment-prod

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   domain: 'nemiac.com/',
   jitBaseUrl: 'pasatrae.com/',
-  baseUrl: 'http://localhost:3001/api/v1/',
+  baseUrl: 'http://198.199.77.142:3001/api/v1/',
 };
